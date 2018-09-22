3a62235654d1a53a0c5db2c9de7c722aab1718fe
Matheus Tusi <matheustusi@gmail.com>
2018-09-22 09:32:03 -0300

    Initial Commit

---
+++ MedidorEnergia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MedidorEnergia
+//
+//  Created by Matheus Santos on 22/09/18.
+//  Copyright © 2018 Matheus Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
